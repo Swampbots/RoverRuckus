0330910c5f98654bf0c72b3775852c5d1dc9a3c2
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-09-22 00:42:37 -0400

    Way too late to be doing this but it woooooorrrrrked

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ItWorrrrrrrrrkeeedddd.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ItWorrrrrrrrrkeeedddd.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ItWorrrrrrrrrkeeedddd {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "It worrrrrrrrrkeeedddd", group = "Testing")
+public class ItWorrrrrrrrrkeeedddd extends OpMode {
+    public void init() {}
+
+
+    public void loop() {
+        telemetry.addLine("I'm aliiiiiiiiiive");
+    }
 }
