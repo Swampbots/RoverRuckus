5f82543ab2ca75987f0cfa6bea91e0df05279205
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-18 23:17:24 -0400

    Added RoverTest driver-control Op Mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class RoverTest extends OpMode {
+
+    RoverHardware hardware = new RoverHardware();
+
+    public void init() {
+        hardware.init(hardwareMap);
+    }
+
+    public void loop() {
+
+    }
+}
