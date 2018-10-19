e8d3891b66c5b7076e7a7c33a7d4bce91cb4998f
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-19 01:23:31 -0400

    Added encoder math for REV HD and Core Hex motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverHardware.java
@@ -9,11 +9,28 @@ public class RoverHardware {
     HardwareMap hwMap;
 
 
+    // Hardware dimensions
+    public static final double WHEEL_CIRCUMFERENCE = 4.0 * Math.PI; // 4-inch diameter * pi = circumference (about 12.57 inches)
+
+
+
 
     // Speed button variables
     public final double SLOW = 0.1;
     public final double NORMAL = 0.2;
     public final double FAST = 0.4;
+
+
+    // Encoder variables (REV Core Hex Motors)
+    public static final double COUNTS_PER_REV_CORE  = 288.0;
+    public static final double GEAR_REDUCTION_CORE  = 4.16; // 4.16 rotations of the motor shaft is 1 rotation of the drive pivot
+    public static final double COUNTS_PER_INCH_CORE = (COUNTS_PER_REV_CORE * GEAR_REDUCTION_CORE) / WHEEL_CIRCUMFERENCE; // To be confirmed
+
+    // Encoder variables (REV HD Hex Motors)
+    public static final double COUNTS_PER_REV_HD   = 1120.0;
+    public static final double GEAR_REDUCTION_HD   = 4.16; // 4.16 rotations of the motor shaft is 1 rotation of the drive pivot
+    public static final double COUNTS_PER_INCH_HD  = (COUNTS_PER_REV_HD * GEAR_REDUCTION_HD) / WHEEL_CIRCUMFERENCE; // To be confirmed
+
 
 
 
