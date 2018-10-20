ee784a4f87351617a6682df03b24cf1044855601
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-20 08:44:59 -0400

    Added experimental wheel pivot controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTest.java
@@ -2,9 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Rover Test", group = "Testing")
 public class RoverTest extends OpMode {
+
+    private final int GP1_A_COOLDOWN = 500;
+    private final int PIVOT_ERROR = 50;
+
+    private double runtimeSnapshot = -GP1_A_COOLDOWN;
+
+    private boolean frontRotForward = true;
+    private boolean rearRotForward = true;
 
     RoverHardware hardware = new RoverHardware();
 
@@ -25,6 +34,58 @@ public class RoverTest extends OpMode {
         else if(gamepad1.dpad_down) hardware.winch.setPower(-1.0);
         else                        hardware.winch.setPower(0);
 
+
+
+//        // START PIVOT CODE
+//
+//
+//        boolean aCooldownOver = ((getRuntime() - runtimeSnapshot) >= GP1_A_COOLDOWN);
+//
+//        if(gamepad1.a && aCooldownOver) {
+//
+//            runtimeSnapshot = getRuntime();
+//
+//            hardware.rearPivot.setTargetPosition    ((int)hardware.STANDARD);
+//            hardware.frontPivot.setTargetPosition   ((int)hardware.STANDARD);
+//
+//            // Both motors move the same direction
+//            frontRotForward = true;
+//            rearRotForward = true;
+//        }
+//
+//        boolean frontNotReached = ( hardware.frontPivot.getTargetPosition() -
+//                hardware.frontPivot.getCurrentPosition() >
+//                PIVOT_ERROR);
+//
+//        boolean rearNotReached = ( hardware.frontPivot.getTargetPosition() -
+//                hardware.frontPivot.getCurrentPosition() >
+//                PIVOT_ERROR);
+//
+//        // If the pivot is not busy and the target is not reached (to exclude case where target is reached)
+//        if(!hardware.frontPivot.isBusy() && frontNotReached) {
+//            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            hardware.frontPivot.setPower(1.0);
+//        } else if(hardware.frontPivot.isBusy() && frontNotReached) { // (still finding target)
+//
+//        } else { // (target reached and pivot is not busy)
+//            hardware.frontPivot.setPower(0.0);
+//            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//
+//        }
+//
+//        if(!hardware.rearPivot.isBusy()) {
+//            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            hardware.rearPivot.setPower(1.0);
+//        } else {
+//            hardware.rearPivot.setPower(0.0);
+//            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//
+//        }
+//
+//        // END PIVOT CODE
+
+
+
         // Motor encoder controls
         if(gamepad2.a)  hardware.resetDriveEncoders();
         if(gamepad2.b)  hardware.resetPivotEncoders();
@@ -35,11 +96,8 @@ public class RoverTest extends OpMode {
 
 
         // Telemetry
-        telemetry.addData("Winch power", hardware.winch.getPower());
-        telemetry.addData("Rear left encoder",      hardware.rearLeft.getCurrentPosition());
-        telemetry.addData("Rear right encoder",     hardware.rearRight.getCurrentPosition());
-        telemetry.addData("Front left encoder",     hardware.frontLeft.getCurrentPosition());
-        telemetry.addData("Front right encoder",    hardware.frontRight.getCurrentPosition());
+        telemetry.addData("Front Pivot power", hardware.frontPivot.getPower());
+        telemetry.addData("Rear Pivot power", hardware.rearPivot.getPower());
         telemetry.addLine();
         telemetry.addData("Front pivot encoder",    hardware.frontPivot.getCurrentPosition());
         telemetry.addData("Rear pivot encoder",     hardware.rearPivot.getCurrentPosition());
