390e896e16c4915b87eedf96342c588ee130ec03
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-20 21:03:12 -0400

    Removed experimental pivot code
    We might return to this later, but for now a method will do (instead of trying to set targets and run to them every cycle of loop()).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTest.java
@@ -7,9 +7,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "Rover Test", group = "Testing")
 public class RoverTest extends OpMode {
 
-
-
-
     RoverHardware hardware = new RoverHardware();
 
     public void init() {
@@ -29,55 +26,7 @@ public class RoverTest extends OpMode {
         else if(gamepad1.dpad_down) hardware.winch.setPower(-1.0);
         else                        hardware.winch.setPower(0);
 
-
-
-//        // START PIVOT CODE
-//
-//
-//        boolean aCooldownOver = ((getRuntime() - runtimeSnapshot) >= GP1_A_COOLDOWN);
-//
-//        if(gamepad1.a && aCooldownOver) {
-//
-//            runtimeSnapshot = getRuntime();
-//
-//            hardware.rearPivot.setTargetPosition    ((int)hardware.STANDARD);
-//            hardware.frontPivot.setTargetPosition   ((int)hardware.STANDARD);
-//
-//            // Both motors move the same direction
-//            frontRotForward = true;
-//            rearRotForward = true;
-//        }
-//
-//        boolean frontNotReached = ( hardware.frontPivot.getTargetPosition() -
-//                hardware.frontPivot.getCurrentPosition() >
-//                PIVOT_ERROR);
-//
-//        boolean rearNotReached = ( hardware.frontPivot.getTargetPosition() -
-//                hardware.frontPivot.getCurrentPosition() >
-//                PIVOT_ERROR);
-//
-//        // If the pivot is not busy and the target is not reached (to exclude case where target is reached)
-//        if(!hardware.frontPivot.isBusy() && frontNotReached) {
-//            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//            hardware.frontPivot.setPower(1.0);
-//        } else if(hardware.frontPivot.isBusy() && frontNotReached) { // (still finding target)
-//
-//        } else { // (target reached and pivot is not busy)
-//            hardware.frontPivot.setPower(0.0);
-//            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//
-//        }
-//
-//        if(!hardware.rearPivot.isBusy()) {
-//            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//            hardware.rearPivot.setPower(1.0);
-//        } else {
-//            hardware.rearPivot.setPower(0.0);
-//            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//
-//        }
-//
-//        // END PIVOT CODE
+        
 
 
 
