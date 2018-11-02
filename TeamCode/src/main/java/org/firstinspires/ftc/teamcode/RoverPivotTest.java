8c08f162f6e58b6a75c98161636dea0c296a4733
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-02 11:11:18 -0400

    Added pivot targets and controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverPivotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverPivotTest.java
@@ -3,10 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STOWED;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STD;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_OMNI;
+
 @TeleOp(name = "Pivot Test", group = "Testing")
 public class RoverPivotTest extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
+
+    private int frontTarget = 0;
+    private int rearTarget = 0;
 
     public void init() {
         hardware.init(hardwareMap);
@@ -21,6 +29,21 @@ public class RoverPivotTest extends OpMode {
         // Wheel subassembly pivots
         hardware.rearPivot.setPower (gamepad2.left_stick_y);
         hardware.frontPivot.setPower(gamepad2.right_stick_y);
+
+        if(gamepad1.a) {
+            frontTarget = PIV_STOWED;
+            rearTarget = PIV_STOWED;
+        } else if(gamepad1.b) {
+            frontTarget = PIV_KNEEL;
+            rearTarget = PIV_KNEEL;
+        } else if(gamepad1.x) {
+            frontTarget = PIV_OMNI;
+            rearTarget = PIV_STD;
+        } else if(gamepad1.y) {
+            frontTarget = PIV_STD;
+            rearTarget = PIV_OMNI;
+        }
+
 
     }
 }
