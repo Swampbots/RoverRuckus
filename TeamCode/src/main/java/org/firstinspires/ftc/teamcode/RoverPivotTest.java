7ac21e91f601d60992e10abb275302de6538efd5
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-02 21:42:00 -0400

    Updated targets to use pivot state variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverPivotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverPivotTest.java
@@ -15,8 +15,10 @@ public class RoverPivotTest extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
 
-    private int frontTarget = PIV_STOWED;
-    private int rearTarget = PIV_STOWED;
+    private int frontTarget = PIV_STOWED[0];
+    private int rearTarget = PIV_STOWED[1];
+
+
 
     public void init() {
         hardware.init(hardwareMap);
@@ -31,23 +33,23 @@ public class RoverPivotTest extends OpMode {
 
 
 
-        // Set wheel pivot powers
-        hardware.rearPivot.setPower (gamepad2.left_stick_y);
-        hardware.frontPivot.setPower(gamepad2.right_stick_y);
+//        // Set pivot powers
+//        hardware.rearPivot.setPower (gamepad2.left_stick_y);
+//        hardware.frontPivot.setPower(gamepad2.right_stick_y);
 
         // Handle pivot targets
         if(gamepad1.a) {
-            frontTarget = PIV_STOWED;
-            rearTarget = PIV_STOWED;
+            frontTarget = PIV_STOWED[0];
+            rearTarget = PIV_STOWED[1];
         } else if(gamepad1.b) {
-            frontTarget = PIV_KNEEL;
-            rearTarget = PIV_KNEEL;
+            frontTarget = PIV_KNEEL[0];
+            rearTarget = PIV_KNEEL[1];
         } else if(gamepad1.x) {
-            frontTarget = PIV_OMNI;
-            rearTarget = PIV_STD;
+            frontTarget = PIV_OMNI[0];
+            rearTarget = PIV_STD[1];
         } else if(gamepad1.y) {
-            frontTarget = PIV_STD;
-            rearTarget = PIV_OMNI;
+            frontTarget = PIV_STD[0];
+            rearTarget = PIV_OMNI[1];
         }
 
         // Set pivot targets
