159d1b362948a1ba98dacb1243afc96b049edb3b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-02 22:18:44 -0400

    Enabled RoverPivotTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverPivotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverPivotTest.java
@@ -10,7 +10,7 @@ import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL;
 import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STD;
 import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_OMNI;
 
-@Disabled
+
 @TeleOp(name = "Pivot Test", group = "Testing")
 public class RoverPivotTest extends OpMode {
 
