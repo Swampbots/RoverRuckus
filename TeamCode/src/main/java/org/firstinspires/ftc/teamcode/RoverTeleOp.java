e5ef91a6cd34b6da4a2cc6a69b548001455e1181
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-16 16:43:21 -0500

    Copied pivot controls from RoverPivotTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTeleOp.java
@@ -6,6 +6,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import static org.firstinspires.ftc.teamcode.RoverHardware.COUNTS_PER_INCH_CORE;
 import static org.firstinspires.ftc.teamcode.RoverHardware.COUNTS_PER_REV_CORE;
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_MINE;
 import static org.firstinspires.ftc.teamcode.RoverHardware.STOP_CLOSED;
 import static org.firstinspires.ftc.teamcode.RoverHardware.STOP_OPEN;
 import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STOWED;
@@ -20,6 +23,20 @@ public class RoverTeleOp extends OpMode {
 
     private final double MINERAL_TOP = 0.0;
     private final double MINERAL_BOTTOM = -650.0;
+
+
+    private int frontTarget = PIV_STOWED[0];
+    private int rearTarget = PIV_STOWED[1];
+
+    private final double PIV_SPEED_BASE = 0.7;
+    private final double PIV_SPEED_FRONT = PIV_SPEED_BASE;
+    private final double PIV_SPEED_REAR = PIV_SPEED_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
+
+    private final int GAMEPAD_SENSITIVITY = 20;
+
+
+
+    
 
     public void init() {
         hardware.init(hardwareMap);
@@ -38,13 +55,38 @@ public class RoverTeleOp extends OpMode {
 
 
         // Pivot motor controls
-//        if(gamepad2.a) setPivots(PIV_STOWED,    0.6);
-//        if(gamepad2.b) setPivots(PIV_OMNI,      0.6);
-//        if(gamepad2.x) setPivots(PIV_KNEEL,     0.6);
-//        if(gamepad2.y) setPivots(PIV_STD,       0.6);
+// Handle pivot targets
+        if(gamepad1.a) {
+            frontTarget = PIV_STOWED[0];
+            rearTarget = PIV_STOWED[1];
+        } else if(gamepad1.b) {
+            frontTarget = PIV_KNEEL[0];
+            rearTarget = PIV_KNEEL[1];
+        } else if(gamepad1.x) {
+            frontTarget = PIV_OMNI[0];
+            rearTarget = PIV_OMNI[1];
+        } else if(gamepad1.y) {
+            frontTarget = PIV_STD[0];
+            rearTarget = PIV_STD[1];
+        } else if(gamepad1.dpad_left) {
+            frontTarget = PIV_MINE[0];
+            rearTarget = PIV_MINE[1];
+        }
 
-        hardware.rearPivot.setPower (gamepad2.left_stick_y);
-        hardware.frontPivot.setPower(gamepad2.right_stick_y);
+        frontTarget += (gamepad2.left_stick_y * GAMEPAD_SENSITIVITY);
+        rearTarget += (gamepad2.right_stick_y * GAMEPAD_SENSITIVITY* (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT));
+
+        // Set pivot targets
+        hardware.frontPivot.setTargetPosition(frontTarget);
+        hardware.rearPivot.setTargetPosition(rearTarget);
+
+        // Set run mode
+        hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Set speed
+        hardware.frontPivot.setPower(PIV_SPEED_FRONT);
+        hardware.rearPivot.setPower(PIV_SPEED_REAR);
 
 
 
