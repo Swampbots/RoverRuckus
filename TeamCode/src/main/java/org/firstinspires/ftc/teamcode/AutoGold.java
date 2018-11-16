bd0342fd31b1f691c37f4bd2c88221a6efa7ac06
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-16 13:30:46 -0500

    Added steps 4.5 and 5 and opModeIsActive() loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGold.java
@@ -90,5 +90,129 @@ public class AutoGold extends LinearOpMode {
 
         driveToPosition(DEPLOY_COUNTS, DRIVE_SPEED);
 
+        // CHANGE TO MINING STATE AND ACTIVATE SNORFLER
+        telemetry.addLine("Step 4.5 (Mining)");
+        telemetry.update();
+        sleep(1000);
+
+        hardware.frontPivot.setTargetPosition(PIV_MINE_FRONT);
+        hardware.rearPivot.setTargetPosition(PIV_MINE_REAR);
+
+        hardware.mineral.setPower(1.0);
+
+
+        // STEP 5
+        telemetry.addLine("Step 5");
+        telemetry.update();
+        sleep(1000);
+
+        driveToPosition(DRIVE_COUNTS, DRIVE_SPEED);
+
+
+
+        // FINISHED
+
+        while(opModeIsActive()) {
+            telemetry.addData("Latch position", hardware.latch.getPosition());
+            telemetry.addData("Drive counts", DRIVE_COUNTS);
+            telemetry.addLine();
+            telemetry.addData("Winch position", hardware.winch.getCurrentPosition());
+            telemetry.addData("fl_drive position", hardware.frontLeft.getCurrentPosition());
+            telemetry.update();
+        }
+
+    }
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // METHODS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public void runToPosition(DcMotor motor, int target, double speed) {
+        motor.setTargetPosition(target);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(speed);
+
+        while(opModeIsActive() && motor.isBusy()) {
+            telemetry.addLine("Deploying to " + motor.getTargetPosition());
+            telemetry.addData("Winch position", motor.getCurrentPosition());
+            telemetry.update();
+        }
+
+        motor.setPower(0);
+
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void deployWheels(double speed) {
+        hardware.frontPivot.setTargetPosition(PIV_STD_FRONT);
+        hardware.rearPivot.setTargetPosition(PIV_OMNI_REAR);
+
+        hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        hardware.frontPivot.setPower(speed);
+        hardware.rearPivot.setPower(speed);
+
+
+//        while (opModeIsActive() && hardware.rearPivot.isBusy()) {
+//            telemetry.addLine("Deploying front to " + hardware.frontPivot.getTargetPosition());
+//            telemetry.addLine("Deploying rear to " + hardware.rearPivot.getTargetPosition());
+//            telemetry.addLine();
+//            telemetry.addData("Front position", hardware.frontPivot.getCurrentPosition());
+//            telemetry.addData("Rear position", hardware.rearPivot.getCurrentPosition());
+//            telemetry.update();
+//        }
+//
+//        hardware.rearPivot.setPower(0);
+//        hardware.rearPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//
+//        while (opModeIsActive() && hardware.frontPivot.isBusy()) {
+//            telemetry.addLine("Deploying front to " + hardware.frontPivot.getTargetPosition());
+//            telemetry.addLine("Deploying rear to " + hardware.rearPivot.getTargetPosition());
+//            telemetry.addLine();
+//            telemetry.addData("Front position", hardware.frontPivot.getCurrentPosition());
+//            telemetry.addData("Rear position", hardware.rearPivot.getCurrentPosition());
+//            telemetry.update();
+//        }
+//
+//        hardware.frontPivot.setPower(0);
+//        hardware.frontPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void driveToPosition(int target, double speed) {
+        hardware.frontLeft.setTargetPosition    (hardware.frontLeft.getCurrentPosition()    + target);
+        hardware.rearLeft.setTargetPosition     (hardware.rearLeft.getCurrentPosition()     + target);
+        hardware.frontRight.setTargetPosition   (hardware.frontRight.getCurrentPosition()   + target);
+        hardware.rearRight.setTargetPosition    (hardware.rearRight.getCurrentPosition()    + target);
+
+        hardware.frontLeft.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearLeft.setMode   (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.frontRight.setMode (DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.rearRight.setMode  (DcMotor.RunMode.RUN_TO_POSITION);
+
+        hardware.frontLeft.setPower (speed);
+        hardware.rearLeft.setPower  (speed);
+        hardware.frontRight.setPower(speed);
+        hardware.rearRight.setPower (speed);
+
+        while(opModeIsActive() &&
+                hardware.frontLeft  .isBusy() &&
+                hardware.rearLeft   .isBusy() &&
+                hardware.frontRight .isBusy() &&
+                hardware.rearRight  .isBusy());
+
+        hardware.frontLeft  .setPower(0);
+        hardware.rearLeft   .setPower(0);
+        hardware.frontRight .setPower(0);
+        hardware.rearRight  .setPower(0);
+
+        hardware.frontLeft.setMode  (DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearLeft.setMode   (DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.frontRight.setMode (DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rearRight.setMode  (DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
