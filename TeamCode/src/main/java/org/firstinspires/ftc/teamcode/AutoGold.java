cc94e7078e771200f4c1b49524e1e3cc1b64e73a
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-16 13:17:45 -0500

    Created AutoGold gold-side autonomus program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGold.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import static org.firstinspires.ftc.teamcode.RoverHardware.LATCH_LEFT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.LATCH_OPEN;
+import static org.firstinspires.ftc.teamcode.RoverHardware.LATCH_RIGHT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_MINE_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_MINE_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_OMNI_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STD_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STD_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.STOP_CLOSED;
+
+@Autonomous(name = "Gold", group = "Autonomous")
+public class AutoGold extends LinearOpMode {
+
+    RoverHardware hardware = new RoverHardware();
+
+    public final double DEPLOY_SPEED = 0.5;
+
+    public final int DEPLOY_COUNTS = 250;
+    public final int DRIVE_COUNTS = 1000;
+    public final double DRIVE_SPEED = 1.0;
+
+
+    public void runOpMode() {
+
+        hardware.init(hardwareMap);
+
+        hardware.latch.setPosition(LATCH_LEFT);
+        hardware.stop.setPosition(STOP_CLOSED);
+
+        telemetry.addLine("Hardware initialized");
+        telemetry.addLine("Press play to start");
+        telemetry.update();
+
+
+
+
+
+        waitForStart();
+        /*
+        STEP 1: release stop
+        STEP 2: deploy wheels
+        STEP 3: open latch
+        STEP 4: drive forward to depot
+        STEP 5: deposit marker
+         */
+
+    }
+}
