0269c01f6834dbef258f2edd7706aac6a4f76715
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-25 17:31:29 -0500

    Added COUNTS_PER_DEGREE calculations for 20:1 motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipperTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipperTest.java
@@ -3,12 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import static org.firstinspires.ftc.teamcode.RoverHardware.COUNTS_PER_REV_HD;
+
 @TeleOp(name = "Flipper Test", group = "Testing")
 public class FlipperTest extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
 
     private final double FLIPPER_SPEED_BASE = 0.5;
+    private final int COUNTS_PER_REV_HD_20 = COUNTS_PER_REV_HD / 2; // Converting from a 40:1 motor to a 20:1
+    private final double GEAR_REDUCTION_FLIPPER = 1.0; // 1:1 gear ratio
+    private final int COUNTS_PER_DEGREE_REV_HD_20 = (int)((COUNTS_PER_REV_HD_20 * GEAR_REDUCTION_FLIPPER) / 480);
+
 
 
     public void init() {
