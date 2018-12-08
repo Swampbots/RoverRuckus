0d5c0fa09ac91cbb1a789c21936d3bda0bffda8a
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-12-08 10:04:53 -0500

    Updated gear ratios

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverHardware.java
@@ -30,8 +30,8 @@ public class RoverHardware {
     // Encoder variables (REV HD Hex Motors)
     public static final int     COUNTS_PER_REV_HD   = 1120;
 
-    public static final double  GEAR_REDUCTION_HD_REAR      = (24.0 / 10.0) * (26.0 / 15.0); // 10-t (output shaft) -> 24-t (transfer shaft) -- 15-t (same shaft) -> 26-t (pivot shaft)
-    public static final double  GEAR_REDUCTION_HD_FRONT     = (24.0 / 10.0) * (26.0 / 15.0); // 10-t (output shaft) -> 24-t (transfer shaft) -- 15-t (same shaft) -> 26-t (pivot shaft)
+    public static final double  GEAR_REDUCTION_HD_REAR      = (24.0 / 16.0) * (26.0 / 15.0); // 16-t (output shaft) -> 24-t (transfer shaft) -- 15-t (same shaft) -> 26-t (pivot shaft)
+    public static final double  GEAR_REDUCTION_HD_FRONT     = (24.0 / 16.0) * (26.0 / 15.0); // 16-t (output shaft) -> 24-t (transfer shaft) -- 15-t (same shaft) -> 26-t (pivot shaft)
 
     public static final int     COUNTS_PER_INCH_HD_REAR     = (int)((COUNTS_PER_REV_HD * GEAR_REDUCTION_HD_REAR) / WHEEL_CIRCUMFERENCE);
     public static final int     COUNTS_PER_INCH_HD_FRONT    = (int)((COUNTS_PER_REV_HD * GEAR_REDUCTION_HD_FRONT) / WHEEL_CIRCUMFERENCE);
