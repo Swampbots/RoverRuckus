156a3752c6bd483b7010a2f5535c07ee63e925b2
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-12-15 23:44:14 -0500

    Replaced FlipperTest code with pivot controls
    (For pivot testing during League Meet 4)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipperTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipperTest.java
@@ -1,11 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import static org.firstinspires.ftc.teamcode.RoverHardware.COUNTS_PER_REV_HD;
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_MINE;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_OMNI;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STD;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STOWED;
 
+//@Disabled
 @TeleOp(name = "Flipper Test", group = "Testing")
 public class FlipperTest extends OpMode {
 
@@ -14,10 +23,25 @@ public class FlipperTest extends OpMode {
     private final double FLIPPER_SPEED_BASE = 0.05;
     private final double FLIPPER_SPEED_SCALER = 0.6;
 
+    int frontTarget = PIV_STOWED[0];
+    int rearTarget = PIV_STOWED[1];
+
 
     private final int COUNTS_PER_REV_HD_20 = COUNTS_PER_REV_HD / 2; // Converting from a 40:1 motor to a 20:1
     private final double GEAR_REDUCTION_FLIPPER = 1.0; // 1:1 gear ratio
     private final int COUNTS_PER_DEGREE_REV_HD_20 = (int)((COUNTS_PER_REV_HD_20 * GEAR_REDUCTION_FLIPPER) / 480);
+
+
+
+    private final double PIV_SPEED_BASE = 0.7;
+    private final double PIV_SPEED_FRONT = PIV_SPEED_BASE;
+    private final double PIV_SPEED_REAR = PIV_SPEED_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
+
+    private final int GAMEPAD_SENSITIVITY = 20;
+
+
+    private final double PIV_SPEED_SCALER_FRONT = 0.6;
+    private final double PIV_SPEED_SCALER_REAR = PIV_SPEED_SCALER_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
 
 
     public int flipperTarget = 0;
@@ -42,19 +66,63 @@ public class FlipperTest extends OpMode {
 //            flipperTarget = hardware.mineral.getCurrentPosition();
 //        }
 
-        double flipperScalar = 0.6;
+//        double flipperScalar = 0.6;
+//
+//        if(gamepad2.a) flipperScalar = 1.0;
+//        else if(gamepad2.b) flipperScalar = 0.4;
+//        else if(gamepad2.x) flipperScalar = 0.6;
+//        else if(gamepad2.y) flipperScalar = 0.8;
+//
+//        hardware.winch.setPower(gamepad2.left_stick_y * flipperScalar);
+//        hardware.mineral.setPower(gamepad2.right_stick_y);
+//
+//
+//        telemetry.addData("Launcher Position", hardware.winch.getCurrentPosition());
+//        telemetry.addData("Flipper Scalar", flipperScalar);
+//        telemetry.update();
 
-        if(gamepad2.a) flipperScalar = 1.0;
-        else if(gamepad2.b) flipperScalar = 0.4;
-        else if(gamepad2.x) flipperScalar = 0.6;
-        else if(gamepad2.y) flipperScalar = 0.8;
-
-        hardware.winch.setPower(gamepad2.left_stick_y * flipperScalar);
-        hardware.mineral.setPower(gamepad2.right_stick_y);
 
 
-        telemetry.addData("Launcher Position", hardware.winch.getCurrentPosition());
-        telemetry.addData("Flipper Scalar", flipperScalar);
-        telemetry.update();
+
+        // Handle pivot targets
+        if(gamepad2.a) {
+            frontTarget = PIV_KNEEL[0];
+            rearTarget = PIV_KNEEL[1];
+        } else if(gamepad2.b) {
+            frontTarget = PIV_STOWED[0];
+            rearTarget = PIV_STOWED[1];
+        } else if(gamepad2.x) {
+            frontTarget = PIV_OMNI[0];
+            rearTarget = PIV_OMNI[1];
+        } else if(gamepad2.y) {
+            frontTarget = PIV_STD[0];
+            rearTarget = PIV_STD[1];
+        } else if(gamepad2.dpad_left) {
+            frontTarget = PIV_MINE[0];
+            rearTarget = PIV_MINE[1];
+        }
+
+        // Set pivot target, run mode, and speed
+        if(Math.abs(gamepad2.right_stick_y) < 0.05) {
+            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            hardware.frontPivot.setPower(PIV_SPEED_FRONT);
+        } else {
+            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            hardware.frontPivot.setPower(-gamepad2.right_stick_y * PIV_SPEED_SCALER_FRONT);
+            frontTarget = hardware.frontPivot.getCurrentPosition();
+        }
+
+        if(Math.abs(gamepad2.left_stick_y) < 0.05) {
+            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            hardware.rearPivot.setPower(PIV_SPEED_REAR);
+        } else {
+            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            hardware.rearPivot.setPower(-gamepad2.left_stick_y * PIV_SPEED_SCALER_REAR);
+            rearTarget = hardware.rearPivot.getCurrentPosition();
+        }
+
+        // Set pivot targets
+        hardware.frontPivot.setTargetPosition(frontTarget);
+        hardware.rearPivot.setTargetPosition(rearTarget);
     }
 }
