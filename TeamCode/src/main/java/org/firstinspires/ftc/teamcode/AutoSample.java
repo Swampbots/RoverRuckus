07267864752ed035280ca8fd17ffbe3c2ddaf6e5
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-08 18:54:34 -0500

    Created AutoSample OpMode
    This is the first step towards sampling in autonomous. I'm going to adapt the code I wrote in GoldContourPipeline and GoldContourPipelineTest into an autonomous mode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSample.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "Sample", group = "Testing")
+public class AutoSample extends OpMode {
+
+    public void init(){
+        
+    }
+
+    public void init_loop() {
+
+    }
+
+    public void loop() {
+
+    }
+}
