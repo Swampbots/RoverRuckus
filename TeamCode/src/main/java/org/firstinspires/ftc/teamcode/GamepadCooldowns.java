5d2b609336f28122c4612eac64a0b591ffcade3c
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-08 20:27:22 -0500

    Created GamepadCooldowns class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadCooldowns.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+public class GamepadCooldowns {
+
+    ButtonCooldown dpUp     = new ButtonCooldown();
+    ButtonCooldown dpDown   = new ButtonCooldown();
+    ButtonCooldown dpLeft   = new ButtonCooldown();
+    ButtonCooldown dpRight  = new ButtonCooldown();
+
+    ButtonCooldown a    = new ButtonCooldown();
+    ButtonCooldown b    = new ButtonCooldown();
+    ButtonCooldown x    = new ButtonCooldown();
+    ButtonCooldown y    = new ButtonCooldown();
+
+    ButtonCooldown lb   = new ButtonCooldown();
+    ButtonCooldown rb   = new ButtonCooldown();
+    ButtonCooldown lt   = new ButtonCooldown();
+    ButtonCooldown rt   = new ButtonCooldown();
+}
