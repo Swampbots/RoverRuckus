7180d29b147a4c54519c84a8330ffe8ab7d9161b
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-09 22:02:29 -0500

    Removed final keyword on COOLDOWN field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonCooldown.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonCooldown.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 public class ButtonCooldown {
 
-    private final double COOLDOWN = 0.075; // 75 milliseconds
+    private double cooldown = 0.075; // 75 milliseconds
     private double snapshot = 0.0;
 
     public void updateSnapshot(double runtime) {
@@ -10,6 +10,6 @@ public class ButtonCooldown {
     }
 
     public boolean ready(double runtime) {
-        return(runtime - snapshot) > COOLDOWN;
+        return(runtime - snapshot) > cooldown;
     }
 }
