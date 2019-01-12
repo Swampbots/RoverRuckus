72b1a9b27317e51574c322d3edb0dea8a84efda4
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-12 17:29:30 -0500

    Created TestLock testing class
    For testing the latching lock

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//@Disabled
+@TeleOp(name = "Test Lock", group ="Testing")
+public class TestLock extends OpMode {
+
+    RoverHardware hardware = new RoverHardware();
+
+    public void init() {
+        hardware.init(hardwareMap);
+    }
+
+    public void loop() {
+
+    }
+}
