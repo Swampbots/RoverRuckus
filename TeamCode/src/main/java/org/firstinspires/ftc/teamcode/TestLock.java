dc4d76f55a8917a161d4f3a9b6444739cd794f42
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-12 18:27:17 -0500

    Renamed TestLock in op mode annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 //@Disabled
-@TeleOp(name = "Test Lock", group ="Testing")
+@TeleOp(name = "Lock Test", group ="Testing")
 public class TestLock extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
