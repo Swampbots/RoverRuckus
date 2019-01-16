31dc845d8dd92e1c65d30d488cc8b921c1a2118c
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-15 23:20:49 -0500

    Added GamepadCooldowns and lock controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
@@ -9,12 +9,32 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class TestLock extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
+    GamepadCooldowns cooldowns = new GamepadCooldowns();
+
+    double runtime = 0.0;
+    final double STEP = 0.05;
 
     public void init() {
         hardware.init(hardwareMap);
+
+        hardware.setLockPosition(0.5);
+
+        cooldowns.setCooldown(0.500); // 500 milliseconds
     }
 
     public void loop() {
 
+        runtime = getRuntime();
+
+        if(gamepad1.a) hardware.setLockPosition(0.0);
+        else if(gamepad1.b)hardware.setLockPosition(0.25);
+        else if(gamepad1.x) hardware.setLockPosition(0.75);
+        else if(gamepad1.y) hardware.setLockPosition(1.0);
+
+
+
+        telemetry.addData("Left lock position", hardware.lockLeft.getPosition());
+        telemetry.addData("Right lock position", hardware.lockRight.getPosition());
+        telemetry.update();
     }
 }
