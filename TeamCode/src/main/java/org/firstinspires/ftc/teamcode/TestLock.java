7558e563f8bbe8a0cc8f48bc32e0c271c9f47f7f
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-17 20:56:21 -0500

    Added pivot controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLock.java
@@ -3,12 +3,29 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STOWED;
 
 //@Disabled
 @TeleOp(name = "Lock Test", group ="Testing")
 public class TestLock extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
+
+
+
+    private int frontTarget = PIV_STOWED[0];
+    private int rearTarget = PIV_STOWED[1];
+
+    private final double PIV_SPEED_BASE = 0.7;
+    private final double PIV_SPEED_FRONT = PIV_SPEED_BASE;
+    private final double PIV_SPEED_REAR = PIV_SPEED_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
+
+    private final double PIV_SPEED_SCALER_FRONT = 0.6;
+    private final double PIV_SPEED_SCALER_REAR = PIV_SPEED_SCALER_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
 
 
     public void init() {
@@ -22,7 +39,28 @@ public class TestLock extends OpMode {
         if(gamepad1.a)      hardware.setLockPosition(0.0);
         else if(gamepad1.b) hardware.setLockPosition(1.0);
 
+        // Set pivot target, run mode, and speed
+        if(Math.abs(gamepad2.right_stick_y) < 0.05) {
+            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            hardware.frontPivot.setPower(PIV_SPEED_FRONT);
+        } else {
+            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            hardware.frontPivot.setPower(-gamepad2.right_stick_y * PIV_SPEED_SCALER_FRONT);
+            frontTarget = hardware.frontPivot.getCurrentPosition();
+        }
 
+        if(Math.abs(gamepad2.left_stick_y) < 0.05) {
+            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            hardware.rearPivot.setPower(PIV_SPEED_REAR);
+        } else {
+            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            hardware.rearPivot.setPower(-gamepad2.left_stick_y * PIV_SPEED_SCALER_REAR);
+            rearTarget = hardware.rearPivot.getCurrentPosition();
+        }
+
+        // Set pivot targets
+        hardware.frontPivot.setTargetPosition(frontTarget);
+        hardware.rearPivot.setTargetPosition(rearTarget);
 
 
 
