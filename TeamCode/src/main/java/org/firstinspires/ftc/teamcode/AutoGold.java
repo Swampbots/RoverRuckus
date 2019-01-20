26b5120706f4443f425fe115ceba90811e9b6ac8
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-20 17:09:53 -0500

    Added AutoGold and AutoSilver

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGold.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+@Autonomous(name = "Gold", group = "Autonomous")
+public class AutoGold extends OpMode {
+
+    public void init() {
+
+    }
+
+    public void init_loop() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void loop() {
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSilver.java
@@ -0,0 +1,23 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+@Autonomous(name = "Silver", group = "Autonomous")
+
+    }
+
+    public void init_loop() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void loop() {
+
+    }
+
+
+
+}
