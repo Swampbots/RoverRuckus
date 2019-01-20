f9250371057de830d309788c8faf58a52f838ade
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-20 17:10:27 -0500

    Added hardware map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSilver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSilver.java
@@ -1,8 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 
 @Autonomous(name = "Silver", group = "Autonomous")
+public class AutoSilver extends OpMode {
+
+    RoverHardware hardware = new RoverHardware();
+
+    public final double RAMP_STOWED = 0.0;
+    public final double LOCK_LOCKED = 0.0;
+
+    public void init() {
+        hardware.init(hardwareMap);
 
     }
 
