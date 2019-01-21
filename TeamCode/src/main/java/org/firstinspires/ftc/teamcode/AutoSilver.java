b363905cff98c7bdf9cb211669a81383400789c8
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-20 19:31:35 -0500

    Added HSV Threshold controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSilver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSilver.java
@@ -79,6 +79,127 @@ public class AutoSilver extends OpMode {
     }
 
     public void init_loop() {
+        //--------------------------------------------------------------------------------------
+        // START HSV THRESHOLD CONTROLS
+        //--------------------------------------------------------------------------------------
+
+            /*
+                CONTROLS: (increase, decrease)
+
+                Hue min: gp1.up,    gp1.down
+                Hue max: gp1.y,     gp1.a
+
+                Sat min: gp1.right, gp1.left
+                Sat max: gp1.b,     gp1.x
+
+                Val min: gp1.lb,    gp1.lt
+                Val max: gp1.rb,    gp1.rt
+             */
+
+        // Modify threshold variables if the buttons are pressed and thresholds are within outer limits 0 & 255
+
+        // Update runtime once every cycle
+        double runtime = getRuntime();
+
+        // HUE MINIMUM
+        if(gamepad1.dpad_down && dpDown.ready(runtime)) {
+            if (hsvHue[0] > HSV_MIN)   hsvHue[0] -= THRESHOLD_STEP;
+            else                        hsvHue[0] = HSV_MIN;
+            dpDown.updateSnapshot(runtime);
+        }
+
+        if(gamepad1.dpad_up && dpUp.ready(runtime)) {
+            if(hsvHue[0] < hsvHue[1])  hsvHue[0] += THRESHOLD_STEP;
+            else                        hsvHue[0] = hsvHue[1];
+            dpUp.updateSnapshot(runtime);
+        }
+
+
+        // HUE MAXIMUM
+        if(gamepad1.y && y.ready(runtime)) {
+            if (hsvHue[1] < HSV_MAX)   hsvHue[1] += THRESHOLD_STEP;
+            else                        hsvHue[1] = HSV_MAX;
+            y.updateSnapshot(runtime);
+        }
+
+        if(gamepad1.a && a.ready(runtime)) {
+            if(hsvHue[1] > hsvHue[0])  hsvHue[1] -= THRESHOLD_STEP;
+            else                        hsvHue[1] = hsvHue[0];
+            a.updateSnapshot(runtime);
+        }
+
+
+
+
+        // SAT MINIMUM
+        if(gamepad1.dpad_left && dpLeft.ready(runtime)) {
+            if (hsvSat[0] > HSV_MIN)   hsvSat[0] -= THRESHOLD_STEP;
+            else                        hsvSat[0] = HSV_MIN;
+            dpLeft.updateSnapshot(runtime);
+        }
+
+        if(gamepad1.dpad_right && dpRight.ready(runtime)) {
+            if(hsvSat[0] < hsvSat[1])  hsvSat[0] += THRESHOLD_STEP;
+            else                        hsvSat[0] = hsvSat[1];
+            dpRight.updateSnapshot(runtime);
+        }
+
+
+        // SAT MAXIMUM
+        if(gamepad1.b && b.ready(runtime)) {
+            if (hsvSat[1] < HSV_MAX)   hsvSat[1] += THRESHOLD_STEP;
+            else                        hsvSat[1] = HSV_MAX;
+            b.updateSnapshot(runtime);
+        }
+
+        if(gamepad1.x && x.ready(runtime)) {
+            if(hsvSat[1] > hsvSat[0])  hsvSat[1] -= THRESHOLD_STEP;
+            else                        hsvSat[1] = hsvSat[0];
+            x.updateSnapshot(runtime);
+        }
+
+
+
+
+        // VAL MINIMUM
+        if(gamepad1.left_trigger > TRIGGER_THRESHOLD && lt.ready(runtime)) {
+            if (hsvVal[0] > HSV_MIN)   hsvVal[0] -= THRESHOLD_STEP;
+            else                        hsvVal[0] = HSV_MIN;
+            lt.updateSnapshot(runtime);
+        }
+
+        if(gamepad1.left_bumper && lb.ready(runtime)) {
+            if(hsvVal[0] < hsvVal[1])  hsvVal[0] += THRESHOLD_STEP;
+            else                        hsvVal[0] = hsvVal[1];
+            lb.updateSnapshot(runtime);
+        }
+
+
+
+        // VAL MAXIMUM
+        if(gamepad1.right_trigger > TRIGGER_THRESHOLD && rt.ready(runtime)) {
+            if (hsvVal[1] > hsvVal[0])  hsvVal[1] -= THRESHOLD_STEP;
+            else                        hsvVal[1] = hsvVal[0];
+            rt.updateSnapshot(runtime);
+        }
+
+        if(gamepad1.right_bumper && rb.ready(runtime)) {
+            if(hsvVal[1] < HSV_MAX)     hsvVal[1] += THRESHOLD_STEP;
+            else                        hsvVal[1] = HSV_MAX;
+            rb.updateSnapshot(runtime);
+        }
+
+        //--------------------------------------------------------------------------------------
+        // END HSV THRESHOLD CONTROLS
+        //--------------------------------------------------------------------------------------
+
+
+
+
+        // SET HSV THRESHOLDS
+        vision.setHsvHue(hsvHue);
+        vision.setHsvSat(hsvSat);
+        vision.setHsvVal(hsvVal);
 
     }
 
