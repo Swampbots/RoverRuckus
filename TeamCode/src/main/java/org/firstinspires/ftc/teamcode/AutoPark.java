5bf2e86d1290afda32bddd6a253fedcb8a074477
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-25 15:53:30 -0500

    Registered AutoPark to autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPark.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@Autonomous(name = "Silver", group = "Autonomous")
 public class AutoPark extends LinearOpMode {
 
     public void runOpMode() {
