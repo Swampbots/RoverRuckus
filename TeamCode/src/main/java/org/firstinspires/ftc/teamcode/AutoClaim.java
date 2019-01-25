3957a86c3f94576c648e720e9e7bfd531302e5fa
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-25 16:56:36 -0500

    Created AutoClaim
    This will serve as our gold-side autonomous for tomorrow.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoClaim.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import static org.firstinspires.ftc.teamcode.RoverHardware.LATCH_OPEN;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.RAMP_DOWN;
+
+public class AutoClaim extends LinearOpMode {
+
+
+    public void runOpMode() {
+        RoverHardware hardware = new RoverHardware();
+
+        hardware.init(hardwareMap);
+
+
+        waitForStart();
+
+        // Descend from lander
+        hardware.setLockPosition(RoverHardware.LOCK_OPEN);
+        sleep(1500);
+        hardware.setLockPosition(RoverHardware.LOCK_CLOSED);
+
+        // Deploy wheels
+        hardware.frontPivot.setTargetPosition(PIV_KNEEL_FRONT);
+        hardware.frontPivot.setTargetPosition(PIV_KNEEL_REAR);
+
+        // Release latch
+        hardware.latch.setPosition(LATCH_OPEN);
+
+        // Lower ramp
+        hardware.ramp.setPosition(RAMP_DOWN);
+
+        // Drive through center sample to crater
+        hardware.driveInches(24.0, 0.8);
+
+        // Snorfle out
+        hardware.snorfler.setPower(1.0);
+
+
+        telemetry.addLine("Finished.");
+        telemetry.update();
+    }
+}
