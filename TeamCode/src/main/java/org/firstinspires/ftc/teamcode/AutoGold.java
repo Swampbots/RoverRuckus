13c10b1d0a1fc387b39ba7f002a5a0c74a4bd646
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-24 20:47:57 -0500

    Disabled AutoGold and AutoSilver

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGold.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-
+@Disabled
 @Autonomous(name = "Gold", group = "Autonomous")
 public class AutoGold extends OpMode {
 
