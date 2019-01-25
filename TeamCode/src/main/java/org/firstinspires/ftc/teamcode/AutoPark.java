3ca73d95a9cfa8bd2504791e2639fcb8fe17fabc
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-25 16:56:17 -0500

    Completed AutoPark

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPark.java
@@ -3,6 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import static org.firstinspires.ftc.teamcode.RoverHardware.LATCH_OPEN;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_KNEEL_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.RAMP_DOWN;
+
 @Autonomous(name = "Silver", group = "Autonomous")
 public class AutoPark extends LinearOpMode {
 
@@ -14,7 +19,29 @@ public class AutoPark extends LinearOpMode {
 
         waitForStart();
 
-        telemetry.addLine("Started!");
+        // Descend from lander
+        hardware.setLockPosition(RoverHardware.LOCK_OPEN);
+        sleep(1500);
+        hardware.setLockPosition(RoverHardware.LOCK_CLOSED);
+
+        // Deploy wheels
+        hardware.frontPivot.setTargetPosition(PIV_KNEEL_FRONT);
+        hardware.frontPivot.setTargetPosition(PIV_KNEEL_REAR);
+
+        // Release latch
+        hardware.latch.setPosition(LATCH_OPEN);
+
+        // Lower ramp
+        hardware.ramp.setPosition(RAMP_DOWN);
+
+        // Snorfle in
+        hardware.snorfler.setPower(-1.0);
+
+        // Drive through center sample to crater
+        hardware.driveInches(24.0, 0.8);
+
+
+        telemetry.addLine("Finished.");
         telemetry.update();
     }
 }
