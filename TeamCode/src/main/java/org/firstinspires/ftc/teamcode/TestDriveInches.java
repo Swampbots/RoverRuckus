b2b1ff51caa03850e83929c80240d4527c5ba913
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-24 20:48:12 -0500

    Added drive encoder telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name = "Test Drive Inches", group = "Testing")
 public class TestDriveInches extends OpMode {
 
     RoverHardware hardware = new RoverHardware();
@@ -16,5 +18,12 @@ public class TestDriveInches extends OpMode {
         if(gamepad1.b)  hardware.driveInches(4.0, 0.8);
         if(gamepad1.x)  hardware.driveInches(-2.0, 0.8);
         if(gamepad1.y)  hardware.driveInches(-4.0, 0.8);
+
+        telemetry.addLine("Encoders:");
+        telemetry.addData("Front left", hardware.frontLeft.getCurrentPosition());
+        telemetry.addData("Front right", hardware.frontLeft.getCurrentPosition());
+        telemetry.addData("Rear left", hardware.frontLeft.getCurrentPosition());
+        telemetry.addData("Rear right.", hardware.frontLeft.getCurrentPosition());
+        telemetry.update();
     }
 }
