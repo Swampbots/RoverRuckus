57aef63571936b3f017d627bd43d848893497de6
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-01-29 17:56:27 -0500

    Added TestBattery testing OpMode
    In an effort to prevent crashes (assumed to be caused by extensive power draw), I plan to research and test different motor actuation methods using this class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBattery.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_FRONT;
+import static org.firstinspires.ftc.teamcode.RoverHardware.GEAR_REDUCTION_HD_REAR;
+import static org.firstinspires.ftc.teamcode.RoverHardware.PIV_STOWED;
+
+@TeleOp(name = "Test Battery", group = "Testing")
+public class TestBattery extends OpMode {
+
+    RoverHardware hardware = new RoverHardware();
+
+    private int frontTarget = PIV_STOWED[0];
+    private int rearTarget = PIV_STOWED[1];
+
+    private final double PIV_SPEED_BASE = 1.0;
+    private final double PIV_SPEED_FRONT = PIV_SPEED_BASE;
+    private final double PIV_SPEED_REAR = PIV_SPEED_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
+
+
+    private final double PIV_SPEED_SCALER_FRONT = 1.0;
+    private final double PIV_SPEED_SCALER_REAR = PIV_SPEED_SCALER_FRONT * (GEAR_REDUCTION_HD_REAR / GEAR_REDUCTION_HD_FRONT);
+
+    public void init() {
+        hardware.init(hardwareMap);
+    }
+
+    public void loop() {
+
+        // Set pivot target, run mode, and speed
+        if(Math.abs(gamepad2.right_stick_y) < 0.05) {
+            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            setThisPower(hardware.frontPivot, PIV_SPEED_FRONT);
+        } else {
+            hardware.frontPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            setThisPower(hardware.frontPivot,-gamepad2.right_stick_y * PIV_SPEED_SCALER_FRONT);
+            frontTarget = hardware.frontPivot.getCurrentPosition();
+        }
+
+        if(Math.abs(gamepad2.left_stick_y) < 0.05) {
+            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            setThisPower(hardware.rearPivot, PIV_SPEED_REAR);
+        } else {
+            hardware.rearPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            setThisPower(hardware.rearPivot, -gamepad2.left_stick_y * PIV_SPEED_SCALER_REAR);
+            rearTarget = hardware.rearPivot.getCurrentPosition();
+        }
+
+        // Set pivot targets
+        hardware.frontPivot.setTargetPosition(frontTarget);
+        hardware.rearPivot.setTargetPosition(rearTarget);
+
+
+        telemetry.addData("Front pivot power", hardware.frontPivot.getPower());
+        telemetry.addData("Rear pivot power", hardware.rearPivot.getPower());
+        telemetry.update();
+    }
+
+
+
+    public void setThisPower(DcMotor motor, double power) {
+
+        motor.getController().setMotorPower(motor.getPortNumber(), power);
+
+    }
+}
