06839e6ca4da9f6b27e9813b573e620a2842c797
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-02-10 16:09:41 -0500

    Disabled TestDriveInches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveInches.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Disabled
 @TeleOp(name = "Test Drive Inches", group = "Testing")
 public class TestDriveInches extends OpMode {
 
