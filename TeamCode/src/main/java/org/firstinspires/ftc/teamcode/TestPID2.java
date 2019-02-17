4a3506a97eff0ab4e75e91f3335789c370622fa3
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-02-16 20:35:05 -0500

    Removed comments
    Yeah, yeah, I know.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPID2.java
@@ -33,10 +33,6 @@ public class TestPID2 extends LinearOpMode {
 
         @Override public void runOpMode() {
 
-
-            // Set up the parameters with which we will use our IMU. Note that integration
-            // algorithm here just reports accelerations to the logcat log; it doesn't actually
-            // provide positional information.
             BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
             parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
             parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -45,21 +41,13 @@ public class TestPID2 extends LinearOpMode {
             parameters.loggingTag          = "IMU";
             parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-            // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-            // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-            // and named "imu".
             imu = hardwareMap.get(BNO055IMU.class, "imu");
             imu.initialize(parameters);
 
-            // Set up our telemetry dashboard
             composeTelemetry();
 
-            // Wait until we're told to go
             waitForStart();
 
-            // Start the logging of measured acceleration
-
-            // Loop and update the dashboard
             while (opModeIsActive()) {
                 telemetry.update();
         }
@@ -71,13 +59,8 @@ public class TestPID2 extends LinearOpMode {
 
         void composeTelemetry() {
 
-        // At the beginning of each telemetry update, grab a bunch of data
-        // from the IMU that we will then display in separate lines.
         telemetry.addAction(new Runnable() { @Override public void run()
         {
-            // Acquiring the angles is relatively expensive; we don't want
-            // to do that in each of the three items that need that info, as that's
-            // three times the necessary expense.
             angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         }
         });
